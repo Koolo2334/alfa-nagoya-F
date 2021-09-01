baa0d299eeda32fd040cb92b4921d957e71e285d
aika-n-toki <89883773+aika-n-toki@users.noreply.github.com>
2021-09-01 10:26:14 +0900

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-d = document;
-function gamestart() {
-    d.querySelector('button#hangon').className = 'on';
-    let scene0to1 = setInterval(function() {
-        d.querySelector('div#call').className = 'go';
-        clearInterval(scene0to1);
-    }, 600);
-}
