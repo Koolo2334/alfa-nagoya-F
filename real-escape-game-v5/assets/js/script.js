e9333df6ee967596325a99900dcc15b5f36dd9f8
koolon <89884320+koolon@users.noreply.github.com>
2021-09-06 10:09:30 +0900

    Add files via upload

---
+++ real-escape-game-v5/assets/js/script.js
@@ -0,0 +1,50 @@
+let countClickStartButton = 0;
+
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*100);
+    });
+}
+
+function disableScroll(event) {
+    event.preventDefault();
+  }
+
+
+$('#btn-start').on('click', async function() {
+    countClickStartButton += 1;
+    if(countClickStartButton === 3){
+        await delay(1);
+        $('#blackscreen').css('display','flex');
+        await delay(1);
+        $('#blackscreen').css('display','none');
+        $('.blackscreen__circle').css('display','none');
+        await delay(3);
+        $('#blackscreen').css('display','flex');
+        document.addEventListener('touchmove', disableScroll, { passive: false });
+        document.addEventListener('mousewheel', disableScroll, { passive: false });
+        $('body').addClass('black-thema');
+        let str = [];
+        $('#blackscreen__statement > span').each(function(i){
+            $(this).css('opacity','1');
+            str[i] = $(this).text();
+            $(this).text('');
+            let no = i;
+            let self = this;
+            let interval = setInterval(async function(){
+                if(no == 0 || $('#blackscreen__statement > span').eq(no - 1).children('span:last').css('opacity') == 1){
+                    clearInterval(interval);
+                    for (var j = 0; j < str[no].length; j++) {
+                        $(self).append('<span>'+str[no].substr(j, 1)+'</span>'); 
+                        $(self).children('span:last').css('opacity','1')
+                        await delay(3);
+                    }
+                }
+            });
+        });
+        await delay(50);
+        document.removeEventListener('touchmove', disableScroll, { passive: false });
+        document.removeEventListener('mousewheel', disableScroll, { passive: false });
+        $('#blackscreen').css('display','none');
+    }
+})
