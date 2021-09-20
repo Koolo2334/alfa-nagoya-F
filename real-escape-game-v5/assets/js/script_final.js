e604fecf9408d7881c5f7ade3fe606f3e0a49b57
koolon <89884320+koolon@users.noreply.github.com>
2021-09-20 11:08:39 +0900

    Update script_final.js

--- real-escape-game-v5/assets/js/script_final.js
+++ real-escape-game-v5/assets/js/script_final.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+    if(location.search === ?q=96) {         //正規ルート
+        
+    }else if(location.search === ?q=99) {   //戻る連打ルート
+        
+    }else if(location.search === ?q=39) {   //ヒント連打ルート
+        
+    }
+}
