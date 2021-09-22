7aac8eb68d28fa6885d83f8fdfcabf851c4e0be1
koolon <89884320+koolon@users.noreply.github.com>
2021-09-22 23:55:53 +0900

    Update script.js

--- real-escape-game-v5/assets/js/script.js
+++ real-escape-game-v5/assets/js/script.js
@@ -31,12 +31,12 @@ window.addEventListener('popstate', function(event) {
                 location.href('/alfa-nagoya-F/real-escape-game-v5/final?q=96');
             }
         }else if(isClearQuiz2) {
-            alertMessage = 'PASSCODEを入力してください。\nKEYWORD : 68498099'
             $('#btn-start--hint').css('display','none');
             $('#btn-start').text('...');
             if(!$('#section--top').hasClass('quiz3')) {
                 $('#section--top').addClass('quiz3');
             }
+            alertMessage = 'PASSCODEを入力してください。\nKEYWORD : 68498099'
         }else{
             if(countReload === 1) {
                 alertMessage = "...";
